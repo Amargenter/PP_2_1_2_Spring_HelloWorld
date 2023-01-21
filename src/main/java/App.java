9eeedbaaf4438554e9db83962413f5195781dae8
Amargenter <amargenter@gmail.com>
2023-01-21 22:49:10 +0300

    First solution 2_1_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Два HelloWorld ссылаются на один объект? " +
+                (bean == bean2));
+        System.out.println("Два Cat ссылаются на один объект? " +
+                (catBean == catBean2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String mew;
+
+
+    public String getMew() {
+        return mew;
+    }
+
+    public void setMew(String mew) {
+        this.mew = mew;
+    }
+}
+
